af6f2f3d4da7ba8061e82f9cfef6b83768228d54
Tikhon Umnov <umnov.tm@gmail.com>
2020-11-05 01:50:10 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Calculate/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Calculate from '@/components/Calculate.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Calculate
+  },
+  methods: {
+
   }
 }
 </script>
 
+<style src="@/assets/styles/reset.css"></style>
 <style>
+* {
+  font-family: 'Gilroy',sans-serif;
+  transition: 0.3s all;
+    font-weight: 600;
+}
+body {
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  background: rgb(30 30 30);
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  width: 200px;
+  box-shadow: 0 3px 15px grey;
 }
 </style>
---
+++ src/components/Calculate.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="grid">
+    <input type="text" v-model="result" placeholder="0">
+    <button class="cell num" @keyup.enter="calc()" @click="input(num)" v-for="num in numbers" :key="num.id">{{ num }}</button>
+    <button class="cell op" @click="input(op)" v-for="op in operations" :key="op.id">{{ op }}</button>
+    <button class="cell op" @click="reset()">R</button>
+    <button class="cell op" @click="calc()">=</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Calculate',
+  data() {
+    return {
+      result: '',
+      numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+      operations: ['+', '-', '*', '/']
+    }
+  },
+  methods: {
+    input(char) {
+      this.result = this.result.toString()
+      this.result+=char
+    },
+    reset() {
+      this.result = ''
+    },
+    calc() {
+      this.result = Math.trunc(eval(this.result))
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+input[type="text"] {
+  width: 200px;
+  border: 0;
+  color: #243849;
+  font-size: 18px;
+  padding: 5px 5px;
+  text-align: right;
+}
+.grid {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  width: 100%;
+}
+.cell {
+  flex: 33%;
+  height: 50px;
+  font-size: 18px;
+  border: none;
+}
+.num {
+  background-color: #243849;
+  color: #fff;
+  font-weight: 400;
+  cursor: pointer;
+}
+.cell:hover{
+  background-color: #182530;
+}
+.op {
+  background-color: #ff9900;
+  color: #243849;
+  cursor: pointer;
+}
+.op:hover {
+  color: #fff;
+}
+
+</style>
